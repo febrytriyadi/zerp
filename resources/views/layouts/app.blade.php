57840999cbf2d1ff7028a1ce418960f6d20138c4
Febry Triyadi <febrytriyadi@example.com>
2026-06-09 11:40:30 +0700

    Update app name to ZERP Febry Triyadi, Aplikasi Keuangan

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'ZERP') }} - Aplikasi Keuangan</title>
+        <title>{{ config('app.name', 'ZERP Febry Triyadi, Aplikasi Keuangan') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
